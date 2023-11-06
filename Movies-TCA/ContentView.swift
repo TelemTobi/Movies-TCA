dcb87f052928920d7efe09f74000bbcca5c2b7d1
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2023-11-06 17:54:34 +0200

    Initial Commit

---
+++ Movies-TCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movies-TCA
+//
+//  Created by Telem Tobi on 06/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movies-TCA/Movies_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movies_TCAApp.swift
+//  Movies-TCA
+//
+//  Created by Telem Tobi on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Movies_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
