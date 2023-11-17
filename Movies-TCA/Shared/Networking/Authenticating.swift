55ba4a65ef0a0f113acc7efd6c66921d3c566290
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2023-11-17 15:57:29 +0200

    Authenticating protocol

--- Movies-TCA/Shared/Networking/Authenticating.swift
+++ Movies-TCA/Shared/Networking/Authenticating.swift
@@ -6,7 +6,19 @@
 //
 
 import Foundation
+import Moya
+
+enum AuthState {
+    case reachable, notReachable, notLoggedIn
+}
 
 protocol Authenticating {
+
+    var authState: AuthState { get }
+
+    // Can be used to asynchronously authenticate the user
+    func authenticate() async throws -> Bool
     
+    // Can be used to map the endpoint right before the request is excecuted
+    func mapEndpoint(_ endpoint: Endpoint, for target: TargetTypeEndPoint) -> Endpoint
 }
--- Movies-TCA/Shared/Networking/Tmdb/TmdbAuthenticator.swift
+++ Movies-TCA/Shared/Networking/Tmdb/TmdbAuthenticator.swift
@@ -6,7 +6,29 @@
 //
 
 import Foundation
+import Moya
 
-class TmdbAuthenticator: Authenticating {
+struct TmdbAuthenticator: Authenticating {
     
+    var authState: AuthState {
+        // TODO: Check network connection
+        return .reachable
+    }
+    
+    func authenticate() async throws -> Bool {
+        return true
+    }
+    
+    func mapEndpoint(_ endpoint: Moya.Endpoint, for target: TargetTypeEndPoint) -> Moya.Endpoint {
+        var headers: [String: String] = [
+            "content-type": "application/json",
+            "Authorization": Config.TmdbApi.accessToken
+        ]
+        
+        target.headers?.forEach { (key, value) in
+            headers[key] = value
+        }
+        
+        return endpoint.adding(newHTTPHeaderFields: headers)
+    }
 }
