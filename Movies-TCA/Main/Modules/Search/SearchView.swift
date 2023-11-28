b06b5cb6b48e75018c8b1073f15dea249d2726dd
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2023-11-28 20:35:14 +0200

    Discover & Search views restructure

--- Movies-TCA/Main/Modules/Search/SearchView.swift
+++ Movies-TCA/Main/Modules/Search/SearchView.swift
@@ -19,7 +19,7 @@ struct SearchView: View {
                     ProgressView()
                 } else {
                     List {
-                        GenresTagsView(genres: viewStore.genres.elements)
+                        SuggestionsView(genres: viewStore.genres.elements)
                             .listRowSeparator(.hidden)
                             .listRowBackground(Color.clear)
                             .listSectionSeparator(.hidden, edges: .top)
@@ -41,31 +41,34 @@ struct SearchView: View {
     }
 }
 
-private struct GenresTagsView: View {
+extension SearchView {
     
-    let genres: [Genre]
-    @State private var didFirstAppear = false
-    
-    var body: some View {
-        let delays = Array(0..<genres.count).map { CGFloat($0) * 0.05 }.shuffled()
+    private struct SuggestionsView: View {
         
-        TagCloudsView(tags: genres.compactMap(\.name)) { index, genre in
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                Text(genre)
-                    .font(.footnote)
-                    .fontWeight(.medium)
+        let genres: [Genre]
+        @State private var didFirstAppear = false
+        
+        var body: some View {
+            let delays = Array(0..<genres.count).map { CGFloat($0) * 0.05 }.shuffled()
+            
+            TagCloudsView(tags: genres.compactMap(\.name)) { index, genre in
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text(genre)
+                        .font(.footnote)
+                        .fontWeight(.medium)
+                }
+                .buttonStyle(TagButtonStyle())
+                .opacity(didFirstAppear ? 1 : 0)
+                .scaleEffect(didFirstAppear ? 1 : 0.7)
+                .rotationEffect(.degrees(didFirstAppear ? 0 : 10))
+                .animation(
+                    .easeInOut(duration: 0.25).delay(delays[index]),
+                    value: didFirstAppear
+                )
             }
-            .buttonStyle(TagButtonStyle())
-            .opacity(didFirstAppear ? 1 : 0)
-            .scaleEffect(didFirstAppear ? 1 : 0.7)
-            .rotationEffect(.degrees(didFirstAppear ? 0 : 10))
-            .animation(
-                .easeInOut(duration: 0.25).delay(delays[index]),
-                value: didFirstAppear
-            )
-        }
-        .onFirstAppear {
-            withAnimation { didFirstAppear = true }
+            .onFirstAppear {
+                withAnimation { didFirstAppear = true }
+            }
         }
     }
 }
