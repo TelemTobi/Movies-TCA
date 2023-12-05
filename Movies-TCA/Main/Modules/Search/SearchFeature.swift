37aeb46fc9279e623818220578d09cf50397668d
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2023-12-05 19:42:26 +0200

    Update SearchFeature.swift

--- Movies-TCA/Main/Modules/Search/SearchFeature.swift
+++ Movies-TCA/Main/Modules/Search/SearchFeature.swift
@@ -18,7 +18,7 @@ struct SearchFeature: Reducer {
         @BindingState var searchInput: String = ""
         
         var isSearchActive: Bool {
-            searchInput.isNotEmpty
+            searchInput.count > 2
         }
     }
     
@@ -26,7 +26,8 @@ struct SearchFeature: Reducer {
         case onFirstAppear
         case onMovieTap(_ movie: Movie)
         case onGenreTap(_ genre: Genre)
-        case resultsResponse(Result<MoviesList, TmdbError>)
+        case searchMovies(_ query: String)
+        case searchResponse(Result<MoviesList, TmdbError>)
         
         case binding(BindingAction<State>)
     }
@@ -41,34 +42,67 @@ struct SearchFeature: Reducer {
             case .onFirstAppear:
                 return .none
                 
-            case .binding(_):
-                return .none
+            case .binding(\.$searchInput):
+                state.isLoading = state.isSearchActive
                 
-            case let .onGenreTap(genre):
-                state.searchInput = genre.name ?? .empty
-                state.isLoading = true
+                guard state.isSearchActive else {
+                    return .none
+                }
+
+                let input = state.searchInput
                 
                 return .run { send in
-                    let discoverResult = await tmdbClient.discoverMovies(genre.id)
-                    await send(.resultsResponse(discoverResult))
+                    try? await Task.sleep(until: .now + .seconds(1))
+                    await send(.searchMovies(input))
                 }
                 
-            case let .resultsResponse(.success(response)):
+            case let .onGenreTap(genre):
+                guard let genreName = genre.name else {
+                    return .none
+                }
+                
+                state.searchInput = genreName
+                state.isLoading = true
+                
+                return .send(.searchMovies(genreName))
+                
+            case let .searchMovies(query):
+                guard query == state.searchInput else {
+                    return .none
+                }
+                
+                print("👀 " + query)
+                if let genre = state.genres.first(where: { $0.name == query}) {
+                    return .run { send in
+                        let discoverResult = await tmdbClient.discoverMovies(genre.id)
+                        await send(.searchResponse(discoverResult))
+                    }
+                    
+                } else {
+                    return .run { send in
+                        let searchResult = await tmdbClient.searchMovies(query)
+                        await send(.searchResponse(searchResult))
+                    }
+                }
+                
+            case let .searchResponse(.success(response)):
                 state.isLoading = false
                 
                 if let movies = response.results {
-                    customDump(movies)
                     state.results = .init(uniqueElements: movies)
                 } else {
-                    return .send(.resultsResponse(.unknownError))
+                    return .send(.searchResponse(.unknownError))
                 }
                 
                 return .none
                 
-            case let .resultsResponse(.failure(error)):
+            case let .searchResponse(.failure(error)):
                 state.isLoading = false
                 
                 customDump(error) // TODO: Handle error
+                return .none
+                
+            case .binding(_):
                 return .none
                 
             // MARK: Handled in parent feature
