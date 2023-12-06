cf00098dc613c396d9fd491da083f5aa24f88a83
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2023-12-06 08:15:14 +0200

    Created Watchlist feature

---
+++ Movies-TCA/Main/Modules/Watchlist/WatchlistView.swift
@@ -0,0 +1,21 @@
+//
+//  WatchlistView.swift
+//  Movies-TCA
+//
+//  Created by Telem Tobi on 06/12/2023.
+//
+
+import SwiftUI
+
+struct WatchlistView: View {
+    
+    var body: some View {
+        Text("WIP")
+            .foregroundStyle(.secondary)
+            .navigationTitle("Watchlist")
+    }
+}
+
+#Preview {
+    WatchlistView()
+}
