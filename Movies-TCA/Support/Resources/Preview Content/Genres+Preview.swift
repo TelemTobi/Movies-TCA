d45e55fb86c0f074e163c4ae08df39a591cb3554
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2023-12-29 18:48:25 +0200

    Create Genres+Preview.swift

---
+++ Movies-TCA/Support/Resources/Preview Content/Genres+Preview.swift
@@ -0,0 +1,36 @@
+//
+//  Genres+Preview.swift
+//  Movies-TCA
+//
+//  Created by Telem Tobi on 29/12/2023.
+//
+
+import Foundation
+
+extension GenresResponse {
+    
+    static var mock: GenresResponse {
+        let response = try? GenresResponse.self
+            .resolve(Mock.listGenres.dataEncoded)
+            .parse(type: GenresResponse.self)
+        
+        guard let response else {
+            fatalError("Movies mock decoding error")
+        }
+        
+        return response
+    }
+}
+
+extension Genre {
+    
+    static var mock: Genre {
+        let genre = GenresResponse.mock.genres?.randomElement()
+        
+        guard let genre else {
+            fatalError("Movies mock decoding error")
+        }
+        
+        return genre
+    }
+}
