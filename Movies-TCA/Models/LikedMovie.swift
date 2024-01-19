d16734ef9c835782b06e4d1d564584e0b2b7f839
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2024-01-19 21:12:26 +0200

    LikedMovie model

---
+++ Movies-TCA/Models/LikedMovie.swift
@@ -0,0 +1,27 @@
+//
+//  LikedMovie.swift
+//  Movies-TCA
+//
+//  Created by Telem Tobi on 19/01/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class LikedMovie {
+    
+    @Attribute(.unique)
+    let id: Int?
+    
+    let title: String?
+    let overview: String?
+    let posterPath: String?
+    
+    init(_ movie: Movie) {
+        self.id = movie.id
+        self.title = movie.title
+        self.overview = movie.overview
+        self.posterPath = movie.posterPath
+    }
+}
