72dc03f31ecaa3e70e3fecabedcc7cdce3b9e019
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2024-03-24 14:11:51 +0200

    MovieFeature adjustments

--- Movies-TCA/Navigators/Movie/MovieNavigator.swift
+++ Movies-TCA/Navigators/Movie/MovieNavigator.swift
@@ -26,11 +26,21 @@ struct MovieNavigator {
         case path(StackAction<Path.State, Path.Action>)
     }
     
+    @Dependency(\.dismiss) private var dismiss
+    
     var body: some ReducerOf<Self> {
         Scope(state: \.root, action: \.root, child: MovieFeature.init)
         
         Reduce { state, action in
             switch action {
+            case let .root(.navigation(.pushRelatedMovie(movie))),
+                 let .path(.element(_, action: .relatedMovie(.navigation(.pushRelatedMovie(movie))))):
+                state.path.append(.relatedMovie(MovieFeature.State(movieDetails: .init(movie: movie))))
+                return .none
+                
+            case .root(.navigation(.dismissFlow)),
+                 .path(.element(_, action: .relatedMovie(.navigation(.dismissFlow)))):
+                return .run { _ in await dismiss() }
                 
             case .root, .path:
                 return .none
