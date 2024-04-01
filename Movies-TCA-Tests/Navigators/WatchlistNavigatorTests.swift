009affb5c0d9118a2370d4987727934a6aa10c65
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2024-04-01 08:10:01 +0300

    Create WatchlistNavigatorTests

---
+++ Movies-TCA-Tests/Navigators/WatchlistNavigatorTests.swift
@@ -0,0 +1,38 @@
+//
+//  WatchlistNavigatorTests.swift
+//  Movies-TCA-Tests
+//
+//  Created by Telem Tobi on 01/04/2024.
+//
+
+import XCTest
+@testable import Movies_TCA
+import ComposableArchitecture
+
+@MainActor
+final class WatchlistNavigatorTests: XCTestCase {
+    
+    var store = TestStore(
+        initialState: WatchlistNavigator.State(),
+        reducer: WatchlistNavigator.init
+    )
+    
+    func testPresentMovie() async {
+        let mockMovie = Movie.mock
+        
+        await store.send(.root(.navigation(.presentMovie(mockMovie)))) { state in
+            state.destination = .movie(MovieNavigator.State(movieDetails: .init(movie: mockMovie)))
+        }
+        
+        await store.send(.destination(.dismiss)) { state in
+            state.destination = nil
+        }
+    }
+    
+    func testPresentPreferences() async {
+        await store.send(.root(.navigation(.presentPreferences))) { state in
+            state.destination = .preferences(PreferencesFeature.State())
+        }
+    }
+}
+
