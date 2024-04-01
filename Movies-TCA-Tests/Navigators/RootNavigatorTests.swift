a7918e0b8add8c3fae3794416886f68ee71b011d
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2024-04-01 07:54:13 +0300

    Create RootNavigatorTests

---
+++ Movies-TCA-Tests/Navigators/RootNavigatorTests.swift
@@ -0,0 +1,25 @@
+//
+//  RootNavigatorTests.swift
+//  Movies-TCA-Tests
+//
+//  Created by Telem Tobi on 01/04/2024.
+//
+
+import XCTest
+@testable import Movies_TCA
+import ComposableArchitecture
+
+@MainActor
+final class RootNavigatorTests: XCTestCase {
+    
+    var store = TestStore(
+        initialState: RootNavigator.State(),
+        reducer: RootNavigator.init
+    )
+    
+    func testSplashCompleted() async {
+        await store.send(.destination(.splash(.navigation(.splashCompleted)))) { state in
+            state.destination = .home(HomeNavigator.State())
+        }
+    }
+}
