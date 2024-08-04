dbe6ae3b452882d6a0230f974c6c7a4561767845
Telem Tobi <telem16@gmail.com>
2024-08-04 19:24:28 +0300

    Create EnvironmentValues extension

---
+++ Movies-TCA/Utility/Extensions/SwiftUI/EnvironmentValues+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  EnvironmentValues+Extension.swift
+//  Movies-TCA
+//
+//  Created by Telem Tobi on 03/08/2024.
+//
+
+import SwiftUI
+
+extension EnvironmentValues {
+    
+    @Entry var movieTransition: Namespace.ID? = nil
+}
